0cb1f91c4e304c255394bfe46b18caf7678da5d8
minodz9 <81865254+minodz9@users.noreply.github.com>
2022-04-30 15:08:19 +0200

    mis à jour

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-let villeChoisie = "Paris";
-recevoirTemperature(villeChoisie);
+let choiceCity = "Paris";
+getTemperature(choiceCity);
 
-let changerDeVille = document.querySelector('#changer');
-changerDeVille.addEventListener('click', () => {
-  villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
-  recevoirTemperature(villeChoisie);
+let changeCity = document.querySelector('#change');
+changeCity.addEventListener('click', () => {
+  choiceCity = prompt('Quelle ville souhaitez-vous voir ?');
+  getTemperature(choiceCity);
 });
 
-function recevoirTemperature(ville) {
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+function getTemperature(city) {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
 
-  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-  requete.open('GET', url); // Nous récupérons juste des données
-  requete.responseType = 'json'; // Nous attendons du JSON
-  requete.send(); // Nous envoyons notre requête
+  let request = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+  request.open('GET', url); // Nous récupérons juste des données
+  request.responseType = 'json'; // Nous attendons du JSON
+  request.send(); // Nous envoyons notre requête
 
   // Dès qu'on reçoit une réponse, cette fonction est executée
-  requete.onload = function() {
-    if (requete.readyState === XMLHttpRequest.DONE) {
-      if (requete.status === 200) {
-        let reponse = requete.response;
+  request.onload = function() {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        let reponse = request.response;
         // console.log(reponse);
         let temperature = reponse.main.temp;
-        let ville       = reponse.name;
+        let city       = reponse.name;
         // console.log(temperature);
         document.querySelector('#temperature_label').textContent = temperature;
-        document.querySelector('#ville').textContent = ville;
+        document.querySelector('#city').textContent = city;
       }
       else {
         alert('Un problème est intervenu, merci de revenir plus tard.');
