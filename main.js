c765737f0de53f4d68e864809ea961ef0cce4d27
minodz9 <81865254+minodz9@users.noreply.github.com>
2022-05-01 18:01:13 +0200

    add geolocation

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
-let choiceCity = "Paris";
-getTemperature(choiceCity);
+let choiceCity;
+// getTemperature(choiceCity);
+if ('geolocation' in navigator) {
+
+  navigator.geolocation.watchPosition(position => {
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+    console.log(url)
+
+    let request = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+    request.open('GET', url); // Nous récupérons juste des données
+    request.responseType = 'json'; // Nous attendons du JSON
+    request.send(); // Nous envoyons notre requête
+
+    // Dès qu'on reçoit une réponse, cette fonction est executée
+    request.onload = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+          let reponse = request.response;
+          console.log(reponse);
+          let temperature = reponse.main.temp;
+          let city = reponse.name;
+          // console.log(temperature);
+          document.querySelector('#temperature_label').textContent = temperature;
+          document.querySelector('#city').textContent = city;
+        }
+        else {
+          alert('Un problème est intervenu, merci de revenir plus tard.');
+        }
+      }
+    }
+
+  }, error, options);
+
+} else {
+
+  changeCity = 'Paris';
+  getTemperature(changeCity);
+}
+
+
+function error() {
+
+  changeCity = 'Paris';
+  getTemperature(changeCity);
+}
+
+var options = {
+
+  enableHighAccuracy: true
+}
 
 let changeCity = document.querySelector('#change');
 changeCity.addEventListener('click', () => {
@@ -16,13 +65,13 @@ function getTemperature(city) {
   request.send(); // Nous envoyons notre requête
 
   // Dès qu'on reçoit une réponse, cette fonction est executée
-  request.onload = function() {
+  request.onload = function () {
     if (request.readyState === XMLHttpRequest.DONE) {
       if (request.status === 200) {
         let reponse = request.response;
-        // console.log(reponse);
+        console.log(reponse);
         let temperature = reponse.main.temp;
-        let city       = reponse.name;
+        let city = reponse.name;
         // console.log(temperature);
         document.querySelector('#temperature_label').textContent = temperature;
         document.querySelector('#city').textContent = city;
